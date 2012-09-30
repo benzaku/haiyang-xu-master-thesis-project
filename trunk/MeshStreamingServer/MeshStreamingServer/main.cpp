641c362d97d0ede8a709ca3ec78944bb2ad2e860
benzaku <benzaku@0b7a3147-44f9-171e-61fe-e1ca7556472f>
2012-09-30 12:51:01 +0000

    add code

---
+++ trunk/MeshStreamingServer/MeshStreamingServer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MeshStreamingServer
+//
+//  Created by Haiyang Xu on 9/30/12.
+//  Copyright (c) 2012 Haiyang Xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
