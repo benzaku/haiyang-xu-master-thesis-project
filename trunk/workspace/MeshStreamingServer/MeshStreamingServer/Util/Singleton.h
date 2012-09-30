ba5d20bc1617fc4db54e4f017879bdb2bfac9af7
benzaku <benzaku@0b7a3147-44f9-171e-61fe-e1ca7556472f>
2012-09-30 14:00:47 +0000

    dd

---
+++ trunk/workspace/MeshStreamingServer/MeshStreamingServer/Util/Singleton.h
@@ -0,0 +1,30 @@
+//
+//  Singleton.h
+//  PMStreamServer
+//
+//  Created by Haiyang Xu on 9/13/12.
+//  Copyright (c) 2012 SJTU. All rights reserved.
+//
+
+#ifndef PMStreamServer_Singleton_h
+#define PMStreamServer_Singleton_h
+
+
+using namespace std ;
+
+class Singleton 
+{
+public:
+    static Singleton* getInstance();
+    
+    ~Singleton();
+    
+private:
+    static bool instanceFlag;
+    static Singleton* single;
+    Singleton();
+};
+
+
+
+#endif
