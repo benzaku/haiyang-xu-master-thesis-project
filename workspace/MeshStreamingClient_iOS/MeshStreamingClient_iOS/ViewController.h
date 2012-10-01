c776d8ed62ca362e76cb74fa878861913e75fd2e
benzaku@gmail.com <benzaku@gmail.com@0b7a3147-44f9-171e-61fe-e1ca7556472f>
2012-10-01 11:08:05 +0000

    import MeshStreamingClient_iOS project

---
+++ workspace/MeshStreamingClient_iOS/MeshStreamingClient_iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MeshStreamingClient_iOS
+//
+//  Created by Haiyang Xu on 01.10.12.
+//  Copyright (c) 2012 Haiyang Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
