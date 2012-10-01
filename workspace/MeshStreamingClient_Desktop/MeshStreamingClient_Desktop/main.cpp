342c54d8d6799d52c6f97d7099d3f7921ac3eb3d
benzaku@gmail.com <benzaku@gmail.com@0b7a3147-44f9-171e-61fe-e1ca7556472f>
2012-10-01 10:44:29 +0000

    add MeshStreamingClient_Desktop project

---
+++ workspace/MeshStreamingClient_Desktop/MeshStreamingClient_Desktop/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MeshStreamingClient_Desktop
+//
+//  Created by Haiyang Xu on 01.10.12.
+//  Copyright (c) 2012 Haiyang Xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
