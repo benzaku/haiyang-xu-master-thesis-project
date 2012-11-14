cf0844a5866ffef05a27b4686c7df41c4154328f
benzaku@gmail.com <benzaku@gmail.com@0b7a3147-44f9-171e-61fe-e1ca7556472f>
2012-11-14 15:43:47 +0000

    add proj

---
+++ workspace/MeshStremingiOSClient/MeshStremingiOSClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeshStremingiOSClient
+//
+//  Created by Xu Haiyang on 11/14/12.
+//  Copyright (c) 2012 Xu Haiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
