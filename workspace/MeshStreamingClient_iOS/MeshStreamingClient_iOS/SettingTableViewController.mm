ebc6c087abb24d3f00f7206df86d87926854a821
benzaku@gmail.com <benzaku@gmail.com@0b7a3147-44f9-171e-61fe-e1ca7556472f>
2012-11-14 15:41:54 +0000

    del

---
+++ workspace/MeshStreamingClient_iOS/MeshStreamingClient_iOS/SettingTableViewController.mm
@@ -0,0 +1,37 @@
+//
+//  SettingTableViewController.m
+//  MeshStreamingClient_iOS
+//
+//  Created by Xu Haiyang on 11/9/12.
+//  Copyright (c) 2012 Haiyang Xu. All rights reserved.
+//
+
+#import "SettingTableViewController.h"
+
+@interface SettingTableViewController ()
+
+@end
+
+@implementation SettingTableViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
