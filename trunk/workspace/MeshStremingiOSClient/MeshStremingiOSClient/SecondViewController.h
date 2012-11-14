20e4a91629693dfc51d9de7ebe5f9be4bf60e0d3
benzaku@gmail.com <benzaku@gmail.com@0b7a3147-44f9-171e-61fe-e1ca7556472f>
2012-11-14 15:43:47 +0000

    add proj

---
+++ trunk/workspace/MeshStremingiOSClient/MeshStremingiOSClient/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MeshStremingiOSClient
+//
+//  Created by Xu Haiyang on 11/14/12.
+//  Copyright (c) 2012 Xu Haiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
