6d4c232ab671d18f634f233808aa7ef651afd77d
benzaku@gmail.com <benzaku@gmail.com@0b7a3147-44f9-171e-61fe-e1ca7556472f>
2012-11-14 17:04:50 +0000

    init refactory

---
+++ trunk/workspace/MeshStremingiOSClient/MeshStremingiOSClient/ProgMeshGLKViewController.mm
@@ -0,0 +1,66 @@
+//
+//  ProgMeshGLKViewController.m
+//  MeshStremingiOSClient
+//
+//  Created by Xu Haiyang on 11/14/12.
+//  Copyright (c) 2012 Xu Haiyang. All rights reserved.
+//
+
+#import "ProgMeshGLKViewController.h"
+
+@interface ProgMeshGLKViewController ()
+
+
+@property (strong, nonatomic) EAGLContext *context;
+@property (strong, nonatomic) GLKBaseEffect *effect;
+@end
+
+@implementation ProgMeshGLKViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"ProgMeshGLKView", @"ProgMeshGLKView");
+        self.tabBarItem.image = [UIImage imageNamed:@"first"];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    [super setPreferredFramesPerSecond:60];
+    
+    self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    
+    if (!self.context) {
+        NSLog(@"Failed to create ES context");
+    }
+    
+    GLKView *view = (GLKView *)self.view;
+    
+    [self.view setMultipleTouchEnabled:YES];
+    
+    view.context = self.context;
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    
+    if ([self isViewLoaded] && ([[self view] window] == nil)) {
+        self.view = nil;
+                
+        if ([EAGLContext currentContext] == self.context) {
+            [EAGLContext setCurrentContext:nil];
+        }
+        self.context = nil;
+    }
+}
+
+@end
