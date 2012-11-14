bc30cc803e7277bfa02cced94b889b1b62908139
benzaku@gmail.com <benzaku@gmail.com@0b7a3147-44f9-171e-61fe-e1ca7556472f>
2012-11-14 15:41:54 +0000

    del

--- trunk/workspace/MeshStreamingClient_iOS/MeshStreamingClient_iOS/AppDelegate.h
+++ trunk/workspace/MeshStreamingClient_iOS/MeshStreamingClient_iOS/AppDelegate.h
@@ -15,4 +15,5 @@
 
 @property (strong, nonatomic) ViewController *viewController;
 
+
 @end
---
+++ trunk/workspace/MeshStreamingClient_iOS/MeshStreamingClient_iOS/SettingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingTableViewController.h
+//  MeshStreamingClient_iOS
+//
+//  Created by Xu Haiyang on 11/9/12.
+//  Copyright (c) 2012 Haiyang Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingTableViewController : UIViewController
+
+@end
