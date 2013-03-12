de48099b4634d9bc1b39ea7ad04b3fd6fc0dd3c8
benzaku@gmail.com <benzaku@gmail.com@0b7a3147-44f9-171e-61fe-e1ca7556472f>
2013-03-12 13:37:37 +0000

    migrate to branches

--- workspace/MeshStremingiOSClient/MeshStremingiOSClient/AdditionalIncludes.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  AdditionalIncludes.cpp
-//  MeshStremingiOSClient
-//
-//  Created by Xu Haiyang on 1/15/13.
-//  Copyright (c) 2013 Xu Haiyang. All rights reserved.
-//
-
-#include "AdditionalIncludes.h"
